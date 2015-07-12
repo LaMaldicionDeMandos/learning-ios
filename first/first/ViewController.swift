1d3d3e5b002c04b540eb5a5c4b0febba943d4dfc
Marcelo Pasut <pasutmarcelo@gmail.com>
2015-07-12 15:37:28 -0300

    first commit

---
+++ first/first/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  first
+//
+//  Created by Marcelo on 7/12/15.
+//  Copyright (c) 2015 Marcelo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
